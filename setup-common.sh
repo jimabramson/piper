49e8aa1054374f71bca14dda009745e442027f55
Jim <downsampling@gmail.com>
2020-08-30 12:42:43 -0400

    add leader setup

---
+++ setup-common.sh
@@ -0,0 +1,28 @@
+### system baseline
+sudo systemctl disable systemd-timesyncd
+sudo systemctl disable apt-daily.service
+sudo systemctl disable apt-daily.timer
+sudo systemctl disable apt-daily-upgrade.timer
+sudo systemctl disable apt-daily-upgrade.service
+
+sudo cp boot/config.txt /boot/config.txt
+
+
+### usb drive
+sudo apt-get install exfat-fuse
+sudo cp etc/fstab /etc/
+
+
+### video player
+
+# scripts
+cp -R pi/. /home/pi/
+
+# omxplayer
+wget https://www.dropbox.com/s/4m38yj2ggosy13e/omxplayer-2018-06-27-raspbian-stretch-pib3%2B.tgz -P /tmp
+sudo tar -C / -zxvf /tmp/omxplayer-2018-06-27-raspbian-stretch-pib3+.tgz
+
+# system service
+sudo cp etc/systemd/system/syncplayer.service /etc/systemd/system/
+sudo systemctl enable syncplayer.service
+
---
+++ setup-leader.sh
@@ -0,0 +1,19 @@
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $SCRIPT_DIR/setup-common.sh
+
+
+### wifi AP
+sudo apt-get install -y hostapd dnsmasq
+sudo systemctl unmask hostapd
+sudo systemctl enable hostapd
+sudo cp etc/dhcpcd.conf /etc/
+sudo cp etc/dnsmasq.conf /etc/
+sudo cp etc/hostapd/hostapd.conf /etc/hostapd/
+sudo cp etc/default/hostapd /etc/default/
+
+
+### ntp follower
+sudo apt-get -y install ntp
+sudo cp etc/ntp.conf /etc/ntp-server.conf
+sudo chown -R ntp /var/log/ntpstats  # TODO not working
+
