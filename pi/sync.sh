54248176e350c00d07c3743bba6cfdd5387eac68
Jim <downsampling@gmail.com>
2020-08-30 17:12:45 -0400

    disable `ntpd -gq`

--- pi/sync.sh
+++ pi/sync.sh
@@ -3,7 +3,7 @@
 if ! systemctl status hostapd; then
     # unless we are the leader (hosting the wifi ap), force an ntp sync
     /bin/systemctl stop ntp
-    /usr/sbin/ntpd -gq
+    # /usr/sbin/ntpd -gq
     /bin/systemctl start ntp
 else
     echo "skipping ntp sync"
